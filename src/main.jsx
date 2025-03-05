d74eb6c511442a26cb90b9892e5dece3a27d3924
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2025-03-05 22:49:30 +0530

    new one

--- src/main.jsx
+++ src/main.jsx
@@ -10,35 +10,6 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import EditResume from './dashboard/resume/[resumeId]/edit/index.jsx'
 import ViewResume from './my-resume/[resumeId]/view/index.jsx'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-const router=createBrowserRouter([
-  {
-    path:'/',
-    element:<Home/>
-  },
-  {
-    element:<App/>,
-    children:[
-      {
-        path:'/dashboard',
-        element:<Dashboard/>
-      },
-      {
-        path:'/dashboard/resume/:resumeId/edit',
-        element:<EditResume/>
-      },
-    ]
-  },
- ,
-  {
-    path:'/auth/sign-in',
-    element:<SignInPage/>
-  },
-  {
-    path:'/my-resume/:resumeId/view',
-    element:<ViewResume/>
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
