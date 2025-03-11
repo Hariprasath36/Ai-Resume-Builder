536f69eba0d10fb0d2e538de90c773fff7051315
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2025-03-11 22:54:01 +0530

    added resumeld

--- src/dashboard/resume/[resumeId]/edit/index.jsx
+++ src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -7,31 +7,5 @@ import dummy from '@/data/dummy';
 import GlobalApi from './../../../../../service/GlobalApi';
 
 function EditResume() {
-    const {resumeId}=useParams();
-    const [resumeInfo,setResumeInfo]=useState();
-    useEffect(()=>{
-       
-        GetResumeInfo();
-    },[])
-
-
-    const GetResumeInfo=()=>{
-        GlobalApi.GetResumeById(resumeId).then(resp=>{
-          console.log(resp.data.data);
-          setResumeInfo(resp.data.data);
-        })
-    }
-
-  return (
-    <ResumeInfoContext.Provider value={{resumeInfo,setResumeInfo}}>
-    <div className='grid grid-cols-1 md:grid-cols-2 p-10 gap-10'>
-        {/* Form Section  */}
-          <FormSection/>
-        {/* Preview Section  */}
-         <ResumePreview/>
-    </div>
-    </ResumeInfoContext.Provider>
-  )
-}
-
+   
 export default EditResume
